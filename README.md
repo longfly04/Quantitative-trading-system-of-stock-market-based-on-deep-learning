618a86f0429ff542e368adc1d3ae29a6582c927d
LongFly <longfly_04@126.com>
2019-12-09 11:23:10 +0800

    add readme and initial the framework

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Quantitative-analysis-with-Deep-Learning
 Quantitative analysis with deep learning prediction and reinforcement learning transactions.
+
+本项目尝试使用深度学习模型预测股价波动，并结合强化学习算法训练一个交易代理，让交易代理通过对模型预测的未来股价、市场现有的交易情况（环境）和先验知识（人工策略）三个方面进行综合决策。
+
+## 股价预测模型
+
+使用基于feature pattern注意力机制的LSTM网络对A股市场股票进行分析，预测未来走势情况。
+
+## 自动量化交易模型
+
+强化学习领域以是否对环境建模，分为model free算法和model based算法。根据本项目中股价预测模型(Feature pattern attetion LSTM)，我们使用model based算法，将强化学习的agent与模型进行交互，同时为了引入人工先验知识，我们尝试将人工策略的行为作为agent的battle，agent同时学习对手的行为方式，从环境模型、策略反馈和对手三个方面获取信息优化自身的策略，提出一套基于battle的actor critic算法模型。
+
+## 参考项目
+
+Awesome DL/RL/SL in Quantitative Finance
+https://github.com/georgezouq/awesome-deep-reinforcement-learning-in-finance#trading-system-back-test--live-trading
+
+
+
+
+
+## 参考文献
+
+PGPortfolio: Policy Gradient Portfolio, the source code of "A Deep Reinforcement Learning Framework for the Financial Portfolio Management Problem"(https://arxiv.org/pdf/1706.10059.pdf).
+
+Uncertainty-driven Imagination for Continuous Deep Reinforcement Learning (http://proceedings.mlr.press/v78/kalweit17a/kalweit17a.pdf)
+
+Model-Based Value Estimation for Efficient Model-Free Reinforcement Learning  (https://arxiv.org/abs/1803.00101)
+
+Value Prediction Network  (https://arxiv.org/abs/1707.03497)
+
+Imagination-Augmented Agents for Deep Reinforcement Learning  (https://arxiv.org/pdf/1707.06203.pdf)
+
+
+Sample-Efficient Reinforcement Learning with Stochastic Ensemble Value Expansion  (https://arxiv.org/abs/1807.01675)
---
+++ util/tools.py
@@ -0,0 +1,76 @@
+import time
+import datetime as dt
+import sys,os
+import logging
+from functools import wraps
+import traceback
+
+class Timer():
+    '''
+    定义一个计时器类 stop方法输出用时
+    '''
+
+    def __init__(self):
+        self.start_dt = None
+
+    def start(self):
+        self.start_dt = dt.datetime.now()
+
+    def stop(self):
+        end_dt = dt.datetime.now()
+        print('[Timer] Time taken: %s' % (end_dt - self.start_dt))
+
+class Logger():
+    '''
+    一个日志记录器
+    '''
+    def __init__(self,):
+        log_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),"logs")
+        today = time.strftime('%Y%m%d', time.localtime(time.time()))
+        full_path = os.path.join(log_dir, today)
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
+        self.log_path = os.path.join(full_path,"quant.log")
+
+    def get_logger(self, ):
+     # 获取logger实例，如果参数为空则返回root logger
+        logger = logging.getLogger("quant")
+        if not logger.handlers:
+            # 指定logger输出格式
+            formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+ 
+            # 文件日志
+            file_handler = logging.FileHandler(self.log_path, encoding="utf8")
+            file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
+ 
+            # 控制台日志
+            console_handler = logging.StreamHandler(sys.stdout)
+            console_handler.formatter = formatter  # 也可以直接给formatter赋值
+ 
+            # 为logger添加的日志处理器
+            logger.addHandler(file_handler)
+            logger.addHandler(console_handler)
+ 
+            # 指定日志的最低输出级别，默认为WARN级别
+            logger.setLevel(logging.INFO)
+     #  添加下面一句，在记录日志之后移除句柄
+        return  logger
+
+
+def info(func):
+    @wraps(func)
+    def log(*args,**kwargs):
+        logger = Logger()
+        try:
+            print("[INFO] Method: \" {name} \" is starting...".format(name = func.__name__))
+            timer = Timer()
+            timer.start()
+            result = func(*args,**kwargs)
+            timer.stop()
+            print("[INFO] Method: \" {name} \" is completed .".format(name = func.__name__))
+            return result
+        except Exception as e:
+            logger.get_logger().error(f"{func.__name__} is error,here are details:{traceback.format_exc()}")
+    return log
+
+
