62a3f9a766bd85a163c3812abc97234bbd9e683f
LongFly <longfly_04@126.com>
2020-01-06 14:55:53 +0800

    add some TODOs

--- train.py
+++ train.py
@@ -1,12 +1,16 @@
 from time import sleep
 from vnpy.app.script_trader import ScriptEngine
+from vnpy.app.script_trader import init_cli_trading
+from vnpy.gateway.xtp import XtpGateway
+from vnpy.event import EventEngine
+from vnpy.trader.engine import MainEngine
 
 def run(engine: ScriptEngine):
     """"""
-    vt_symbols = ["IF1912.CFFEX", "rb2001.SHFE"]
+    vt_symbols = ["600036.SSE", "601601.SSE", "000060.SZSE", "000002.SZSE"]
 
     # 订阅行情
-    engine.subscribe(vt_symbols)
+    # engine.subscribe(vt_symbols)
 
     # 获取合约信息
     for vt_symbol in vt_symbols:
@@ -25,6 +29,30 @@ def run(engine: ScriptEngine):
         # 等待3秒进入下一轮
         sleep(3)
 
+def main():
+    setting = {
+    "账号": "53191000110",
+    "密码": "Lw6t1JVS",
+    "客户号": 1,
+    "行情地址": "120.27.164.138",
+    "行情端口": 6002,
+    "交易地址": "120.27.164.69",
+    "交易端口": 6001,
+    "行情协议": "TCP",
+    "授权码": "b8aa7173bba3470e390d787219b2112e"
+    }
+    event_engine = EventEngine()
+    main_engine = MainEngine(event_engine)
+    main_engine.add_gateway(XtpGateway)
+    # main_engine.start()
 
-if __name__ == main():
-    run()
+    engine = init_cli_trading([XtpGateway])
+    engine.connect_gateway(setting,"XTP")
+    # main_engine.add_engine(engine)
+    
+    pass
+    # run(engine)
+
+
+if __name__ == "__main__":
+   main()
